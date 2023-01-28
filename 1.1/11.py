bd59b70b557943f08c3b1b3d53865a42ccaca310
Georgiy Glinskikh <glnskkh@vk.com>
2023-01-28 22:41:00 +0500

    Done some of new tasks

--- 1.1/11.py
+++ 1.1/11.py
@@ -1,18 +1,27 @@
-def test_rev_fact():
-    assert 1.0 == rev_fact(0)
-    assert 1.0+1.0 == rev_fact(1)
-    assert 1.0+1.0+0.5 == rev_fact(2)
-    assert 1.0+1.0+0.5+(1/6.0) == rev_fact(3)
+def test_slow_rev_fact():
+    assert 1.0 == slow_rev_fact(0)
+    assert 1.0+1.0 == slow_rev_fact(1)
+    assert 1.0+1.0+0.5 == slow_rev_fact(2)
+    assert 1.0+1.0+0.5+(1/6.0) == slow_rev_fact(3)
 
 
-def rev_fact(n: int):
-    d = 1.0
-    sum = 1.0
+def fact(n: int):
+    "Стандартная реализация вычисления факториала за O(n)"
+    p: int = 1
 
-    # Инвариант sum = 1/0! + ... 1/(k - 1)!
-    for k in range(n):
-        d /= float(k + 1)
-        sum += d
+    for k in range(2, n + 1):
+        p *= k
 
-    # Учитывая инвариант и k = n+1 => sum - искомое число
+    return p
+
+
+def slow_rev_fact(n: int):
+    "Сложность: O(n^2)"
+    sum: float = 0
+
+    # sum = 1/0! + 1/1! + ... + 1/(k - 1)!
+    for k in range(n + 1):
+        sum += 1 / fact(k)
+
+    # k = n + 1 =>
     return sum
