07a5afae22fa65c4ede4a7a0e81113e34f3377bd
georgiyglinskikh <glnskkh@vk.com>
2023-02-06 22:12:04 +0500

    Сделана 18я задача

---
+++ 1.2/test_18.py
@@ -0,0 +1,28 @@
+def test_count_undecreacing():
+    assert count_undecreacing([1, 1, 2, 3, 4], [1, 2, 4]) == 3
+
+
+def count_undecreacing(a: list[int], b: list[int]) -> int:
+    k = 0
+
+    # k - число одинаковых элементов в a[0..i] и b[0..j]
+    # a[i], b[j] - элементы, которых еще не встречалось
+
+    i, j = 0, 0
+
+    while not (i == len(a) or j == len(b)):
+        if a[i] < b[j]:
+            i += 1
+        elif a[i] > b[j]:
+            j += 1
+        else:
+            k += 1
+            t = a[i]
+
+            while i < len(a) and a[i] == t:
+                i += 1
+
+            while j < len(b) and b[j] == t:
+                j += 1
+
+    return k
