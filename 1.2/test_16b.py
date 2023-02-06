59939b9cbb9bc5096399313a970e9f15277b2efc
georgiyglinskikh <glnskkh@vk.com>
2023-02-06 13:01:12 +0500

    Сделана 16я задача

--- 1.2/test_16b.py
+++ 1.2/test_16b.py
@@ -1,25 +1,81 @@
+def test_poly_sum():
+    assert poly_sum([1, 1, 1], [1, 1]) == [2, 2, 1]
+
+
+def test_poly_shift():
+    assert poly_shift([1, 1, 1], 2) == [0, 0, 1, 1, 1]
+
+
 def test_fast_poly_mult():
     assert fast_poly_mult([1, 1], [1, 1]) == [1, 2, 1]
-    assert fast_poly_mult([1, -1], [1, 1, 1]) == [1, 0, 0, -1]
+
+
+def poly_sum(a: list[float], b: list[float]) -> list[float]:
+    k = max(len(a), len(b))
+
+    r = [.0] * k
+
+    l = k - 1
+    while l >= 0:
+        r[l] += a[l] if l < len(a) else 0
+        r[l] += b[l] if l < len(b) else 0
+
+        l -= 1
+
+    return r
+
+
+def poly_sub(a: list[float], b: list[float]) -> list[float]:
+    r = [-i for i in b]
+
+    return poly_sum(a, r)
+
+
+def poly_shift(x: list[float], n: int) -> list[float]:
+    return ([.0] * n) + x[:]
+
+
+def poly_mult_const(x: list[float], c: float) -> list[float]:
+    r = x[:]
+
+    for i in range(len(r)):
+        r[i] *= c
+
+    return r
 
 
 def fast_poly_mult(a: list[float], b: list[float]) -> list[float]:
+    "Быстрый алгоритм умножения многочленов степеней 2^n"
     # По аналогии с 16a.py
-    k = max(len(a), len(b)) - 1
 
-    for i in range(k - len(a)):
-        a.append(0)
-    for i in range(k - len(b)):
-        b.append(0)
+    if len(a) <= 1:
+        m = a[0] if len(a) == 1 else 0
+        return poly_mult_const(b, m)
 
-    A = a[:0]
-    B = a[0:]
-    C = b[:0]
-    D = b[0:]
+    if len(b) <= 1:
+        m = b[0] if len(b) == 1 else 0
+        return poly_mult_const(a, m)
+
+    k = min(len(a), len(b)) // 2
+
+    A = a[:k]
+    B = a[k:]
+
+    C = b[:k]
+    D = b[k:]
 
     AC = fast_poly_mult(A, C)
     BD = fast_poly_mult(B, D)
-    sumAB = [A[i] + B[i] for i in range(k//2)]
-    sumCD = [C[i] + D[i] for i in range(k//2)]
-    mid = fast_poly_mult(sumAB, sumCD)
-    mid = [for i in range(k)]
+
+    S1 = poly_sum(A, B)
+    S2 = poly_sum(C, D)
+    MID = fast_poly_mult(S1, S2)
+    MID = poly_sub(MID, AC)
+    MID = poly_sub(MID, BD)
+
+    r = poly_shift(AC, k)
+    r = poly_sum(r, MID)
+    r = poly_shift(r, k)
+    r = poly_sum(r, BD)
+
+    return r
