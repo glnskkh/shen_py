c39215269dc2085a4b0bc73891d6dc0fbfdbf611
georgiyglinskikh <glnskkh@vk.com>
2023-02-09 15:27:49 +0500

    Сделана 26я задача, небольшие правки к 25й

---
+++ 1.2/test_26.py
@@ -0,0 +1,29 @@
+"""
+Необходимо найти общий элемент всех подмассивов
+двумерного массива размером n*m
+Сложность: O(nm)
+"""
+
+
+def test_find():
+    assert find([[1, 2, 3], [1, 2, 3], [1, 4, 5]]) == 1
+
+
+def find(list: list[list[int]]) -> int:
+    indecies = [0] * len(list)
+
+    while True:
+        for i in range(1, len(indecies)):
+            while list[i][indecies[i]] < list[0][indecies[0]]:
+                indecies[i] += 1
+
+        found = True
+        for i in range(1, len(indecies)):
+            found = found and list[i][indecies[i]] == list[0][0]
+
+        if not found:
+            indecies[0] += 1
+        else:
+            break
+
+    return list[0][indecies[0]]
